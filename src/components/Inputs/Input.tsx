87100635c4728498d78980655b3a800ef63bc0fd
Welington Andrade do Carmo <welingtonaroeira@gmail.com>
2025-02-04 20:43:17 -0300

    design da screen toAdd

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -33,13 +33,14 @@ export function Input({ setValue, message, leftIcon, rightIcon, ...rest }: Input
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 1,
     justifyContent: 'space-between',
     height: 82,
     marginTop: 12,
   },
   row: {
     flexDirection: 'row',
-    height: 60,
+    height: 52,
     backgroundColor: MyTheme.colors.background_dark,
     borderRadius: 8,
     paddingHorizontal: 15,
---
+++ src/utils/Descriptions.ts
@@ -0,0 +1,32 @@
+export const ENTRY_DESCRIPTION = [
+  {
+    id: 1,
+    title: 'salário',
+    icon: ''
+  },
+  {
+    id: 2,
+    title: 'venda de imóvel',
+    icon: ''
+  },
+  {
+    id: 3,
+    title: 'venda de automóvel',
+    icon: ''
+  },
+  {
+    id: 4,
+    title: 'venda de objeto',
+    icon: ''
+  },
+  {
+    id: 5,
+    title: 'pagamento de dividendo',
+    icon: ''
+  },
+  {
+    id: 6,
+    title: 'recebimento de aluguel',
+    icon: ''
+  }
+]
